4d9c3a23ddad5e0d99f973e3c773a7e4d82f5735
Dennis <dennissackmann@web.de>
2021-03-10 19:34:26 +0100

    zeitspanne hinzugefügt (manuell)

--- content.js
+++ content.js
@@ -1,10 +1,48 @@
 var url = window.location;
+const time = new Date()
 
+
+const hour = time.getHours();
+const minutes = time.getMinutes();
+const seconds = time.getSeconds();
+
+const start1 = 193000;
+const end1 = 193300;
+const start2 = 074000;
+const end2 = 125000;
+
+var x = ""
+
+if (hour.toString().length == 2) {
+    x = x + hour.toString()
+} else {
+    x = x + hour.toString()
+}
+if (minutes.toString().length == 2) {
+    x = x + minutes.toString()
+} else {
+    x = x + minutes.toString()
+}
+if (seconds.toString().length == 2) {
+    x = x + seconds.toString()
+} else {
+    x = x + seconds.toString()
+}
 
 var danger = ["twitter", "instagram", "github", "youtube", "medium", "stackoverflow"]
 
-for (var i = 0; i <danger.length; i++) {
-    if (window.location.href.indexOf(danger[i]) > -1) {
-        window.location.replace('http://www.blankwebsite.com/');
+
+if ( start1 <=  parseInt(x) && parseInt(x)< end1 ) {
+    for (var i = 0; i <danger.length; i++) {
+        if (window.location.href.indexOf(danger[i]) > -1) {
+            window.location.replace('http://www.blankwebsite.com/');
+        }
+    }
+} else if(start2 <=  parseInt(x) && parseInt(x)< end2) {
+    for (var i = 0; i <danger.length; i++) {
+        if (window.location.href.indexOf(danger[i]) > -1) {
+            window.location.replace('http://www.blankwebsite.com/');
+        }
     }
 }
+
