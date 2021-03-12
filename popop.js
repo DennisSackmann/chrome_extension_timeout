c4855a20b99ae9176327fe8c7a8806431bb3c34f
Dennis <dennissackmann@web.de>
2021-03-12 15:22:01 +0100

    updated files

--- popop.js
+++ popop.js
@@ -7,3 +7,24 @@ checkbox.addEventListener('change', function() {
       alert('unchecked');
   }
 });
+
+
+
+window.onload = function() {
+  document.getElementById("button").onclick = function() {
+    var value = document.getElementById("value_input").value;
+    // alert(value);
+    var x = Date.now();
+    chrome.storage.sync.set({x:value}, function() {
+      alert("Sucess")
+    });
+  }
+
+  document.getElementById("getButton").onclick = function() {
+    chrome.storage.sync.get("x", function(data) {
+      console.log(data.x)
+    });
+  }
+
+
+}
