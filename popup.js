6dc296afcb6a5cd859a561d06a16064050dec2e2
Dennis <dennissackmann@web.de>
2021-03-20 17:28:35 +0100

    `

--- popup.js
+++ popup.js
@@ -40,13 +40,11 @@ function loadWebsite() {
                 center.appendChild(node);
                 center.scrollTo(0, center.scrollHeight);
             } catch (error) {
-                console.log(error)
+                console.log(error);
                 var x = {"website": []}
-                chrome.storage.sync.set({"website": x}), function() {
-                    console.log("Message: error while savig data");
-                }
-                document.getElementById("website_input").value = "";
+                chrome.storage.sync.set({"website":x})
             }
+            
         })
     })
     center.appendChild(node);
@@ -61,10 +59,73 @@ function loadWebsite() {
 function loadTimeline() {
     var center = document.getElementById("center");
     center.innerHTML = "";
+    //header
     var node = document.createElement("h1");
-    var textnode = document.createTextNode("Die Timeline Seite");
+    var textnode = document.createTextNode("Timeline Page");
     node.appendChild(textnode);
     node.setAttribute("class", "center_header")
+    center.appendChild(node);
+    //time 1
+    var node = document.createElement("input");
+    node.setAttribute('type', 'time');
+    node.setAttribute("class", "time_input");
+    node.setAttribute("id", "firsttime");
+    center.appendChild(node);
+    //time 2
+    var node = document.createElement("input");
+    node.setAttribute('type', 'time');
+    node.setAttribute("class", "time_input");
+    node.setAttribute("id", "secondtime");
+    center.appendChild(node);
+    //button
+    var node = document.createElement("button");
+    var textnode = document.createTextNode("add time");
+    node.setAttribute("class", "time_button")
+    node.appendChild(textnode);
+    node.addEventListener("click", function() {
+        t1 = document.getElementById("firsttime").value;
+        t2 = document.getElementById("secondtime").value;
+        if (t1 && t2 && t1<t2) {
+            var first_time = "";
+            var second_time = "";
+            if (t1.toString().startsWith("0")) {
+                time = t1.toString().slice(1).split(":");
+                first_time = time[0] + time[1]
+                console.log(first_time);
+            } else {
+                time = t1.toString().split(":");
+                first_time = time[0] + time[1]
+                console.log(first_time)
+            }
+            if (t2.toString().startsWith("0")) {
+                time = t2.toString().slice(1).split(":");
+                second_time = time[0] + time[1]
+                console.log(second_time);
+            } else {
+                time = t2.toString().split(":");
+                second_time = time[0] + time[1]
+                console.log(second_time);
+            }
+            chrome.storage.sync.get("time", function(data) {
+                console.log(data);
+                try {
+                    console.log(data.time.time.push({"f1": first_time, "f2": second_time}));
+                    var new_data = data;
+                    chrome.storage.sync.set({"time": new_data.time}), function() {
+                        console.log("Message: data saved")
+                    }
+                } catch (error) {
+                    console.log(error);
+                    var x = {"time": []}
+                    chrome.storage.sync.set({"time":x})
+                }
+            })
+            t1 = document.getElementById("firsttime").value = null;
+            t2 = document.getElementById("secondtime").value = null;
+        } else {
+            console.log("Message: input not valid");
+        };
+    })
     center.appendChild(node);
 }
 
@@ -72,7 +133,6 @@ function loadTimeline() {
 
 window.onload = function() {
     loadWebsite();
-
 
     document.getElementById("website").onclick = function() {
         loadWebsite();
